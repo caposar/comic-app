18447c732366a910e3325ba210941cc074b5e068
Martin <mapnlp@gmail.com>
2021-09-26 03:53:18 -0300

    RouterLink y RouterLinkActive
    RouterLink y RouterLinkActive - Completando las rutas

--- ng-comic-app/src/app/app-routing.module.ts
+++ ng-comic-app/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent}, // De esta manera nos evitamos la redireccion
   {path: 'about', component: AboutComponent},
+  {path: 'heroes', component: HeroesComponent},
   {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
